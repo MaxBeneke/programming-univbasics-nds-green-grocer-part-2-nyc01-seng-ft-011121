3c772ef23a3a7cee2b9102aecc14a09ca90b173b
Max Beneke <max.beneke@gmail.com>
2020-12-29 14:38:57 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,43 +4,43 @@ require 'pry'
 def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
 updated_cart = []
-  cart.each do |original_item|
+  cart.each do |cart_item|
     nil_checker = true
     coupons.each do |coupon_item|
-      if original_item[:item] == coupon_item[:item]
+      if cart_item[:item] == coupon_item[:item]
        
         # APPLY coupon if they have exact number
-        if original_item[:count] == coupon_item[:num]
+        if cart_item[:count] == coupon_item[:num]
           with_coupon_hash = {}
           
-          with_coupon_hash[:item] = original_item[:item] + ' W/COUPON'
-          with_coupon_hash[:price] = (coupon_item[:cost]/original_item[:count]).round(2)
-          with_coupon_hash[:clearance] = original_item[:clearance]
-          with_coupon_hash[:count] = original_item[:count]
+          with_coupon_hash[:item] = cart_item[:item] + ' W/COUPON'
+          with_coupon_hash[:price] = (coupon_item[:cost]/cart_item[:count]).round(2)
+          with_coupon_hash[:clearance] = cart_item[:clearance]
+          with_coupon_hash[:count] = cart_item[:count]
         updated_cart.push(with_coupon_hash)
-        original_item[:count] -= coupon_item[:num]
+        cart_item[:count] -= coupon_item[:num]
         nil_checker = false
       end
       ### APPLY coupon if they have extra items
      
-      if original_item[:count] > coupon_item[:num]
+      if cart_item[:count] > coupon_item[:num]
         
-        coupon_multiplier = (original_item[:count] / coupon_item[:num]).floor
-        extra_items = original_item[:count] % coupon_item[:num]
+        coupon_multiplier = (cart_item[:count] / coupon_item[:num]).floor
+        extra_items = cart_item[:count] % coupon_item[:num]
         
           ### Push W/ Coupon Item 
           with_coupon_hash = {}
-          with_coupon_hash[:item] = original_item[:item] + " W/COUPON"
+          with_coupon_hash[:item] = cart_item[:item] + " W/COUPON"
           with_coupon_hash[:price] = (coupon_item[:cost]/coupon_item[:num]).round(2)
-          with_coupon_hash[:clearance] = original_item[:clearance]
+          with_coupon_hash[:clearance] = cart_item[:clearance]
           with_coupon_hash[:count] = coupon_multiplier * coupon_item[:num]
           updated_cart.push(with_coupon_hash)
-          original_item[:count] -= coupon_item[:num]
+          cart_item[:count] -= coupon_item[:num]
           nil_checker = false
           
           if extra_items != 0 
-            original_item[:count] = extra_items
-            updated_cart.push(original_item)
+            cart_item[:count] = extra_items
+            updated_cart.push(cart_item)
             nil_checker = false
           end
           
